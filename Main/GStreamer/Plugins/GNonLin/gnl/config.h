9699fe3e30a926b6a3e98342b2b57a3319568af8
ylatuya <ylatuya@72a73a14-1cf0-11de-a027-6f5b8e30e268>
2009-07-02 16:10:42 +0000

    Each module must use it's own config.h file to setup properly the package name and the packes's version. With the previous settings, gstreamer.vsprops adds the 'common' folder to additional includes dir, and all the properties ineherits from this one. Instead, ech module should take it's config.h from win32/common/config.h or, if not present, from the one we generate. This way each element uses the right config.h file.
    $gst-inspect matroskamux
    
    Before:
    Plugin Details:
      Name:                 matroska
      Description:          Matroska stream handling
      Filename:             C:\gstreamer\lib\gstreamer-0.10/libgstmatroska.dll
      Version:              0.10.23
      License:              LGPL
      Source module:        gst-plugins-base
      Binary package:       GStreamer Base Plug-ins source release
      Origin URL:           Unknown package origin
    
    
    Now:
    
    Plugin Details:
      Name:                 matroska
      Description:          Matroska stream handling
      Filename:             C:\gstreamer\lib\gstreamer-0.10/libgstmatroska.dll
      Version:              0.10.15
      License:              LGPL
      Source module:        gst-plugins-good
      Binary package:       GStreamer Plugins Goog
      Origin URL:           Unknown package origin
    
    
    git-svn-id: http://ossbuild.googlecode.com/svn/trunk@248 72a73a14-1cf0-11de-a027-6f5b8e30e268

---
+++ Main/GStreamer/Plugins/GNonLin/gnl/config.h
@@ -0,0 +1,98 @@
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Default audio sink */
+#define DEFAULT_AUDIOSINK "directsound"
+
+/* Default audio source */
+#define DEFAULT_AUDIOSRC "dshowaudiosrc"
+
+/* Default video sink */
+#define DEFAULT_VIDEOSINK "directdrawsink"
+
+/* Default video source */
+#define DEFAULT_VIDEOSRC "dshowvideosrc"
+
+/* Default visualizer */
+#undef DEFAULT_VISUALIZER
+
+/* Defined if gcov is enabled to force a rebuild due to config.h changing */
+#undef GST_GCOV_ENABLED
+
+/* Default errorlevel to use */
+#undef GST_LEVEL_DEFAULT
+
+/* GStreamer license */
+#define GST_LICENSE "LGPL"
+
+/* package name in plugins */
+#define GST_PACKAGE_NAME "GStreamer GNonLin plugin"
+
+/* package origin */
+#define GST_PACKAGE_ORIGIN "freedesktop.gstreamer.net"
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#undef HAVE_DLFCN_H
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#undef HAVE_INTTYPES_H
+
+/* Define to 1 if you have the <memory.h> header file. */
+#undef HAVE_MEMORY_H
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#undef HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#undef HAVE_STRINGS_H
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#undef HAVE_SYS_TYPES_H
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#undef HAVE_UNISTD_H
+
+/* Define if valgrind should be used */
+#undef HAVE_VALGRIND
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#undef LT_OBJDIR
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+#undef NO_MINUS_C_MINUS_O
+
+/* Name of package */
+#define PACKAGE "gnonlin"
+
+/* Define to the address where bug reports for this package should be sent. */
+#undef PACKAGE_BUGREPORT
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GStreamer GNonLin"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GStreamer GNonLin 0.10.11"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "gnonlin"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.10.11"
+
+/* directory where plugins are located */
+#define PLUGINDIR "\\lib\\gstreamer-0.10"
+
+/* Define to 1 if you have the ANSI C header files. */
+#undef STDC_HEADERS
+
+/* Version number of package */
+#define VERSION "0.10.11"
