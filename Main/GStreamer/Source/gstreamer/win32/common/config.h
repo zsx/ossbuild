5ca26fcae3c23e731e1753832af880381648c280
ylatuya <ylatuya@72a73a14-1cf0-11de-a027-6f5b8e30e268>
2009-10-21 21:44:37 +0000

    Fix version number
    git-svn-id: http://ossbuild.googlecode.com/svn/trunk@338 72a73a14-1cf0-11de-a027-6f5b8e30e268

--- Main/GStreamer/Source/gstreamer/win32/common/config.h
+++ Main/GStreamer/Source/gstreamer/win32/common/config.h
@@ -286,13 +286,13 @@
 #define PACKAGE_NAME "GStreamer"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "GStreamer 0.10.23.3"
+#define PACKAGE_STRING "GStreamer 0.10.25"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "gstreamer"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.10.23.3"
+#define PACKAGE_VERSION "0.10.25"
 
 /* directory where plugins are located */
 #ifdef _DEBUG
@@ -308,7 +308,7 @@
 #undef USE_POISONING
 
 /* Version number of package */
-#define VERSION "0.10.23.3"
+#define VERSION "0.10.25"
 
 /* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
    significant byte first (like Motorola and SPARC, unlike Intel). */
